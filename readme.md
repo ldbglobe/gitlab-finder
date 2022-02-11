d3af6b56f6db0111c488266dbe92582e99f8da57
Mathieu Delestre <ldbglobe@gmail.com>
2022-02-11 14:14:57 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Cli Gitlab Finder
+
+Due to the Gitlab advanced search rate limiter, I was unable to use the CLI tools written by [Phillip Johnsen](https://github.com/phillipj/gitlab-search). So I wrote my own in NodeJS using the GitLab API NodeJS library [GitBeaker by Justin Dalrymple](https://github.com/jdalrymple/gitbeaker)
+
+This project is quite straightforward with only to settings to fill in the .env file (a read only personnal auth token) and the string to search. Then you can start searching by running the ``search`` script
+
+Notice: due to the really low rate limit (10 request per minute) you may wait for quite a long time. Based on my own experiment, about 700 seconds to search across 100 projects
+
+## 1. Install
+
+```bash
+git clone git@github.com:ldbglobe/gitlab-finder.git
+cd gitlab-finder
+yarn
+cp .env.sample .env
+```
+
+## 2. Configure
+
+Edit the ``.env`` to put your own personnal authtoken, search string and file extension (optionnal) before executing the script
+
+## 3. Execute
+
+```bash
+yarn search
+```
---
+++ src/search.js
@@ -0,0 +1,94 @@
+require('dotenv').config()
+
+const fs = require('fs');
+const path = require('path');
+const slugify = require('slugify')
+
+const { Gitlab } = require('@gitbeaker/node');
+const api = new Gitlab({
+  token: process.env.API_TOKEN,
+});
+
+async function getGroups()
+{
+	return await api.Groups.all({
+		perPage:100
+	});
+}
+async function getGroupProjects(group)
+{
+	return await api.Groups.projects(group.id, {
+		perPage:100,
+		include_subgroups:false,
+	});
+}
+
+async function searchInProject(project,search)
+{
+	try {
+		return await api.Search.all("blobs",search,{
+			projectId:project.id,
+			perPage:100,
+		});
+	} catch(e) {
+		console.log(e);
+		process.exit();
+	}
+}
+
+(async function(){
+
+	const resultFile = path.resolve('./',`search-results__${slugify(process.env.SEARCH_KEYWORD)}.json`);
+
+	var searchResults = {};
+	if(fs.existsSync(resultFile))
+	{
+		try {
+			searchResults = JSON.parse(fs.readFileSync(resultFile));
+		} catch(e) {
+		}
+	}
+
+	let groups = await getGroups();
+	for(let i=0; i<groups.length; i++)
+	{
+		let group = groups[i];
+		let projects = await getGroupProjects(group);
+		for(let j=0; j<projects.length; j++)
+		{
+			let project = projects[j];
+			//console.log(project);process.exit;
+			console.log('------------------------------------------------------------');
+			console.log(project.name_with_namespace);
+
+			if(searchResults[project.id])
+			{
+				console.log(' > Allready processed');
+				continue;
+			}
+			else
+			{
+				searchResults[project.id] = searchResults[project.id] || {
+					id: project.id,
+					name:project.name_with_namespace,
+					chunks:[],
+				}
+				await new Promise(resolve => setTimeout(resolve, 6000));
+
+				let filter = [process.env.SEARCH_KEYWORD]
+				if(process.env.SEARCH_FILE_EXTENSION)
+					filter.push('filename:*.'+process.env.SEARCH_FILE_EXTENSION);
+
+				let results = await searchInProject(project,filter.join(' '));
+				for(let k=0; k<results.length; k++)
+				{
+					let element = results[k];
+					console.log('+ '+element.path);
+					element.data = element.data.replace("\t","  ").split("\n");
+					searchResults[project.id].chunks.push(element);
+				}
+				fs.writeFileSync(resultFile, JSON.stringify(searchResults, null, 2));
+			}
+		}
+	}
+})();
